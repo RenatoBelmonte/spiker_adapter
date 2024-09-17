0a95cf9aa3c9e5c90928a2f51b452d41a77fbdab
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-17 16:14:29 +0200

    feat(driver):implemented read memory function

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "pulp.h"
+
+#define BUFFER_SIZE 98
+
+int main()
+{
+    printf("Hello World!\n");
+
+    FILE *file = fopen("./spikes.txt", "rb");
+    if (file == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    uint8_t buffer[98];
+    size_t bytesRead = fread(buffer, sizeof(uint8_t), 98, file);
+    if (bytesRead != 98) {
+        perror("Failed to read enough data");
+        fclose(file);
+        return 1;
+    }
+
+    fclose(file);
+
+    read_from_memory(buffer);
+    
+
+}
---
+++ spiker_adapter_driver.c
@@ -0,0 +1,21 @@
+#include "spiker_adapter.h"
+#include <stdint.h>
+#include <stddef.h>
+
+#define SPIKER_ADAPTER_SPIKES_BASE_ADDR 0x1A402000
+
+void read_from_memory(uint8_t *buffer)
+{
+    uint8_t volatile *spiker_adapter_reg = (uint8_t *)SPIKER_ADAPTER_SPIKES_0_REG_OFFSET;
+    // Read from memory
+    for (size_t i = 0; i < SPIKER_ADAPTER_SPIKES_MULTIREG_COUNT; i++)
+    {
+        spiker_adapter_reg[i] = buffer[i];
+    }
+}
+
+void start_spiker_adapter()
+{
+    // Start the spiker adapter
+
+}
