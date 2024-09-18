6407952e02c609e8e396917e6d2f36506ae947ab
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-18 17:36:42 +0200

    Refactor spiker_adapter_driver.c to add print statements for debugging purposes

--- spiker_adapter_driver.c
+++ spiker_adapter_driver.c
@@ -8,8 +8,10 @@ void read_from_memory(uint8_t *buffer)
 {
     uint8_t volatile *spiker_adapter_reg = (uint8_t *)SPIKER_ADAPTER_SPIKES_BASE_ADDR;
     // Read from memory
+    printf("I'm in the function\n");
     for (size_t i = 0; i < SPIKER_ADAPTER_SPIKES_MULTIREG_COUNT; i++)
     {
+        printf("I'mwriting\n");
         spiker_adapter_reg[i] = buffer[i];
     }
 }
