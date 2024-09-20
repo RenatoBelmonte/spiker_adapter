38f6d9dd0350ff231f4a2a4bcdd74c465d67e7e6
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-20 19:04:29 +0200

    Refactor main.c to limit loop iterations, improve debug output, and update buffer values

--- main.c
+++ main.c
@@ -18,16 +18,18 @@ int main()
     printf("I'm in the function\n");
     uint32_t volatile *spiker_adapter_reg = (uint32_t *)0x1A400000;
     // Read from memory
-    for (size_t i = 0; i < 5; i++)
+    for (size_t i = 0; i < 24; i++)
     {
         printf("\t  Reg = %x \t now ", spiker_adapter_reg[i]);
-        printf("I'm writing buffer = %x\t", buffer[i] + i);
-        spiker_adapter_reg[i] = buffer[i] + i;
+        printf("I'm writing buffer = %x\t", buffer[i] + i + 1);
+        spiker_adapter_reg[i] = buffer[i] + i + 1;
+        asm volatile ("": : : "memory");
         printf("Check: reg = %x\n", spiker_adapter_reg[i]);
     }
     //read_from_memory(buffer);
     //start_spiker_adapter(); 
     
     printf("JOB DONE\n");
+    asm volatile ("": : : "memory");
 
 }
