3b5ef9b7fb0c92b72a8590983a496b2c19e66b38
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-23 15:10:44 +0200

    Refactor spiker_unwrap.sv to update DATA_WIDTH calculation

--- rtl/spiker_unwrap.sv
+++ rtl/spiker_unwrap.sv
@@ -13,8 +13,8 @@ module spiker_unwrap
   import spiker_adapter_reg_pkg::* ;
 
     // Define the DATA_WIDTH based on the number of spikes and their width
-    localparam real width_real = N_SPIKES / WIDTH;
-    localparam int DATA_WIDTH = int'(N_SPIKES / WIDTH);
+    real width_real = N_SPIKES / WIDTH;
+    int DATA_WIDTH = int'(N_SPIKES / WIDTH);
 
     initial begin
         if (width_real < int'(width_real)) begin
