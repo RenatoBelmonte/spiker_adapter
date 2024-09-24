86bd25ca0c5f4329ecae3f46feae276cebd0906b
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-24 19:14:46 +0200

    Refactor printf statements in main.c to update control register for sample ready bit and provide more detailed information about buffer address and control register changes

--- main.c
+++ main.c
@@ -20,38 +20,44 @@ int main()
 
     uint32_t volatile *spiker_adapter_ctrl1 = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_CTRL1_REG_OFFSET);
     uint32_t volatile *spiker_adapter_status = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_STATUS_REG_OFFSET);
-    printf("I'm reading the status and control register\n SAMPLE_READY = %x\t, READY = %x\t SAMPLE = %x\n",\
-     (*spiker_adapter_ctrl1)&(SPIKER_ADAPTER_CTRL1_MASK << SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT),\
-     (*spiker_adapter_ctrl1)&(SPIKER_ADAPTER_CTRL1_MASK << SPIKER_ADAPTER_CTRL1_READY_BIT),\
-      *spiker_adapter_status);
-    asm volatile ("": : : "memory");
-    uint32_t old_ctrl1 = *spiker_adapter_ctrl1;
-    asm volatile ("": : : "memory");
-    uint32_t new_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
-    *spiker_adapter_ctrl1 = new_ctrl1;
-    asm volatile ("": : : "memory");
-    printf("I want to write: %x\nChange the control register from: %x to: %x\n", new_ctrl1,\
-    old_ctrl1, *spiker_adapter_ctrl1);
+    u_int32_t old_ctrl1 = *spiker_adapter_ctrl1;
+
+    *spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
 
 
-//    printf("I'm in the function\n");
-//    uint32_t volatile *spiker_adapter_reg = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_SPIKES_0_REG_OFFSET);
-//    uint32_t volatile *spiker_adapter_res = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_SPIKES_RESULT_0_REG_OFFSET);
-//
-//    // Read from memory
-//    for (size_t i = 0; i < 24; i++)
-//    {
-//        printf("\t  Reg = %x \t now ", spiker_adapter_res[i]);
-//        printf("I'm writing buffer = %x\t", buffer[i] + i + 1);
-//        spiker_adapter_reg[i] = buffer[i] + i + 1;
-//        asm volatile ("": : : "memory");
-//        printf("Check: reg = %x\n", spiker_adapter_res[i]);
-//    }
-//    //read_from_memory(buffer);
-//    //start_spiker_adapter(); 
-//    
-//    printf("JOB DONE\n");
-//    asm volatile ("": : : "memory");
+    printf("I'm in the function\n");
+    uint32_t volatile *spiker_adapter_reg = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_SPIKES_0_REG_OFFSET);
+    uint32_t volatile *spiker_adapter_res = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_SPIKES_RESULT_0_REG_OFFSET);
+
+    while (!spiker_adapter_status)
+    {
+        printf("Waiting for the accelerator to be ready\n");
+    }
+    
+    // Read from memory
+    for (size_t i = 0; i < 24; i++)
+    {
+        printf("\t  Reg = %x \t now ", spiker_adapter_res[i]);
+        printf("I'm writing buffer = %x\t", buffer[i] + i + 1);
+        spiker_adapter_reg[i] = buffer[i] + i + 1;
+        asm volatile ("": : : "memory");
+    }
+    *spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
+    *spiker_adapter_ctrl1 = old_ctrl1 & (0xFFFFFFFE);
+    asm volatile ("": : : "memory");
+    while (!spiker_adapter_status)
+    {
+        printf("Waiting for the accelerator to be ready\n");
+    } 
+    for (size_t i = 0; i < 24; i++)
+    {
+        printf("Check: reg = %x\n", spiker_adapter_res[i]);
+    }
+    //read_from_memory(buffer);
+    //start_spiker_adapter(); 
+    
+    printf("JOB DONE\n");
+    asm volatile ("": : : "memory");
 
     return 0;
 }
