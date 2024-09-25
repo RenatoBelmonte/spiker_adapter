65e78350fcd9c587cf2ce78ab014ba0a7545b600
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-25 17:49:39 +0200

    Reorganization and (fixing) ? sample ready instead of ready at line 31

--- main.c
+++ main.c
@@ -18,17 +18,23 @@ int main()
     memset(buffer, 0, sizeof (buffer));
     printf("The address of buffer is %i\n", &buffer);
 
+    // Set up the pointers to the registers
     uint32_t volatile *spiker_adapter_ctrl1 = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_CTRL1_REG_OFFSET);
     uint32_t volatile *spiker_adapter_status = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_STATUS_REG_OFFSET);
-    uint32_t old_ctrl1 = *spiker_adapter_ctrl1;
-
-    *spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
-
-
-    printf("I'm in the function\n");
     uint32_t volatile *spiker_adapter_reg = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_SPIKES_0_REG_OFFSET);
     uint32_t volatile *spiker_adapter_res = (uint32_t *)(SPIKER_ADAPTER_BASE_ADDR + SPIKER_ADAPTER_SPIKES_RESULT_0_REG_OFFSET);
+    
+     
+    //uint32_t old_ctrl1 = *spiker_adapter_ctrl1;
+    //*spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
 
+    // READY <= 1
+    uint32_t old_ctrl1 = *spiker_adapter_ctrl1;
+    *spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
+    
+    printf("I'm in the function\n");
+
+    // CHECK STATUS OF THE ACCELERATOR
     while (!spiker_adapter_status)
     {
         printf("Waiting for the accelerator to be ready\n");
@@ -42,17 +48,28 @@ int main()
         spiker_adapter_reg[i] = buffer[i] + i + 1;
         asm volatile ("": : : "memory");
     }
+
+    // SAMPLE_READY <= 1
     *spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
-    *spiker_adapter_ctrl1 = old_ctrl1 & (0xFFFFFFFE);
-    asm volatile ("": : : "memory");
+    
+    // CHECK STATUS OF THE ACCELERATOR
     while (!spiker_adapter_status)
     {
         printf("Waiting for the accelerator to be ready\n");
     } 
+
+    // READY <= 0
+    *spiker_adapter_ctrl1 = old_ctrl1 & (0xFFFFFFFE);
+
+    // READ FROM MEMORY
     for (size_t i = 0; i < 24; i++)
     {
         printf("Check: reg[%i] = %x\n", i, spiker_adapter_res[i]);
     }
+
+    // READY <= 1
+    *spiker_adapter_ctrl1 = old_ctrl1 | ((1 & SPIKER_ADAPTER_CTRL1_MASK)<<SPIKER_ADAPTER_CTRL1_SAMPLE_READY_BIT);
+    
     //read_from_memory(buffer);
     //start_spiker_adapter(); 
     
